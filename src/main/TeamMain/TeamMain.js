83d2cc5f2d358508fa2fec3e72a50e06eca562f8
Antonio <38987773+houdiniam@users.noreply.github.com>
2023-10-05 13:47:25 -0300

    Update TeamMain.js

--- src/main/TeamMain/TeamMain.js
+++ src/main/TeamMain/TeamMain.js
@@ -3,11 +3,7 @@ import {
     TeamContainer, TeamH1, TeamWrapper, 
     TeamCard, TeamIcon, TeamH2, TeamP
 } from 'main/TeamMain/TeamElements'
-import Adriel from '../../assets/img/team/drico.jpg'
-import Tinin from '../../assets/img/team/tin.jpg'
-import Antonio from '../../assets/img/team/antonio.jpeg'
-import Nat from '../../assets/img/team/nat.jpg'
-import Rod from '../../assets/img/team/rod.jpg'
+import Team from '../../assets/img/team/image.png'
 
 const ServicesMain = () => {
     return (
@@ -15,29 +11,29 @@ const ServicesMain = () => {
             <TeamH1>Nossa Equipe</TeamH1>
             <TeamWrapper>
                 <TeamCard>
-                    <TeamIcon src={Adriel}/>
-                    <TeamH2>Adriel</TeamH2>
-                    <TeamP>Take it slow o que, carai?</TeamP>
+                    <TeamIcon src={Team}/>
+                    <TeamH2>Team</TeamH2>
+                    <TeamP>Team</TeamP>
                 </TeamCard>
                 <TeamCard>
-                    <TeamIcon src={Tinin}/>
-                    <TeamH2>C. Tinin</TeamH2>
-                    <TeamP>Quem tem um mamão tem tudo.</TeamP>
+                    <TeamIcon src={Team}/>
+                    <TeamH2>Team</TeamH2>
+                    <TeamP>Team</TeamP>
                 </TeamCard>
                 <TeamCard>
-                    <TeamIcon src={Antonio}/>
-                    <TeamH2>Marcos</TeamH2>
-                    <TeamP>Stream &apos; Put it on repeat &apos; da Sabrina.</TeamP>
+                    <TeamIcon src={Team}/>
+                    <TeamH2>Team</TeamH2>
+                    <TeamP>Team</TeamP>
                 </TeamCard>
                 <TeamCard>
-                    <TeamIcon src={Nat}/>
-                    <TeamH2>Natália</TeamH2>
-                    <TeamP>A maioral, primeira de seu nome.</TeamP>
+                    <TeamIcon src={Team}/>
+                    <TeamH2>Team</TeamH2>
+                    <TeamP>Team</TeamP>
                 </TeamCard>
                 <TeamCard>
-                    <TeamIcon src={Rod}/>
-                    <TeamH2>Rodrigo</TeamH2>
-                    <TeamP>O casal 21!</TeamP>
+                    <TeamIcon src={Team}/>
+                    <TeamH2>Team</TeamH2>
+                    <TeamP>Team</TeamP>
                 </TeamCard>
             </TeamWrapper>
         </TeamContainer>
